ad7fb2f205d996017b3408ed54ef4d8646eb0ba2
SaiAdithya3 <saiadithyakancharla@gmail.com>
2024-05-11 20:42:00 +0530

    ok

--- server/index.js
+++ server/index.js
@@ -5,10 +5,15 @@ import connectDB from './db/connect.js';
 import authRoutes from './routes/authRoutes.js';
 import messageRoutes from './routes/messageRoutes.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+}));
 
 const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
