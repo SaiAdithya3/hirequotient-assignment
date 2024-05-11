dd375f7e5c1ba3901af0af3b0e319585cbc158d3
SaiAdithya3 <saiadithyakancharla@gmail.com>
2024-05-11 14:09:49 +0530

    mong

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
@@ -7,6 +10,14 @@ const PORT = 3000;
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('Connected to MongoDB 🥳');
+}).catch((err) => {
+    console.log(err);
+}
+);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}! 🎉`);
