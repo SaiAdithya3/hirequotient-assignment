6d0ba385aa30314f34d7fc18ebcd288928a02006
SaiAdithya3 <saiadithyakancharla@gmail.com>
2024-05-11 17:54:32 +0530

    /

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 const App = () => {
   return (
     <>
       <div className="w-full flex">
-        Chat Application
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </div>
     </>
   )
